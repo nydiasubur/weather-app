7fe8c4c35f4a6a4b0928bbe0b2221ee709840a15
nydia-subur <165659692+nydia-subur@users.noreply.github.com>
2024-04-24 13:55:05 +0800

    connected to github and netlify

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Weather />
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/nydiasubur/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Nydia Subur
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/nydiasubur/weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="weather-app-ns8.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Weather() {
+  return <h3>Weather application</h3>;
+}
